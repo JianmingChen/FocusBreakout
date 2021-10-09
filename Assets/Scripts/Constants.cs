1e80171c90a478349ac06fd0ce9c5f9a49a57a0d
Charles <charleshuang97@gmail.com>
2021-10-09 06:37:53 -0700

    bullet will only fire if blob is in line of sight through Raycast

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,6 @@
 // For constants that can be used across classes.
 public static class Constants {
     public const float Gravity = -9.81f;
+    // Range for RaycastHit
+    public const int HitRange = 1000;
 }
--- Assets/Scripts/HazardManager.cs
+++ Assets/Scripts/HazardManager.cs
@@ -7,26 +7,32 @@ public class HazardManager : MonoBehaviour
     [SerializeField] private float _minFireRate;
     [SerializeField] private float _maxFireRate;
     private BlobManager _blobManager;
+    private Vector3 _startPos;
+    private RaycastHit hitData;
 
     void Start() {
-        // Blob prefab won't let me drag BlobManager into SerializeField so to be consistent 
-        // I used Find() here aswell.
-        _blobManager = GameObject.Find("BlobManager").GetComponent<BlobManager>(); ;
-        StartCoroutine(BulletCoroutine(new Vector3(1, 0, -20)));
+        // Blob prefab won't let me drag BlobManager into its SerializeField
+        // so to be consistent I used Find() here aswell.
+        _blobManager = GameObject.Find("BlobManager").GetComponent<BlobManager>();
+        _startPos = new Vector3(1, 0, -20);
+        StartCoroutine(BulletCoroutine(_startPos));
     }
 
-    void Update() {}
+    private void Update() {
+        Debug.DrawRay(_startPos, _bullet.Direction);
+    }
 
-    private void UpdateBlobList() {}
-
-    // Uses Unity's Random.Range(), values are swapped if _maxFireRate is less than _minFireRate
     private IEnumerator BulletCoroutine(Vector3 startPos) {
         while (true) {
             float time = Random.Range(_minFireRate, _maxFireRate);
 
             yield return new WaitForSeconds(time);
             _bullet.Direction = _blobManager.GetRandomBlobLocation() - startPos;
-            Instantiate(_bullet, startPos, Quaternion.identity);
+            // Only shoot if Raycast hits a blob
+            if (Physics.Raycast(startPos, _bullet.Direction, out hitData, Constants.HitRange)
+                && hitData.collider.tag == "Blob") {
+                Instantiate(_bullet, startPos, Quaternion.identity);
+            }
         }
     }
 
